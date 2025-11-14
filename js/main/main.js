342d9da99f007b3cc146301565279726524bf1ab
mijaster <misareakcia@gmail.com>
2025-11-14 22:03:54 +0500

    s

--- js/main/main.js
+++
@@ -1,9 +0,0 @@
-// js/main/main.js
-document.addEventListener('DOMContentLoaded', () => {
-    if (typeof initHeaderScroll === 'function') initHeaderScroll();
-    if (typeof initAnimations === 'function') initAnimations();
-    if (typeof loadComponents === 'function') loadComponents();
-});
-
-// Прелоадер инициализируется отдельно (ждёт полной загрузки)
-if (typeof initPreloader === 'function') initPreloader();
